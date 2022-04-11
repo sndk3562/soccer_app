a0a57c51cc535bc84a1bafa74dfc1bdef765173e
sndk3562 <nekonoinotora@gmail.com>
2022-04-11 17:38:10 +0900

    tyre

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(ssl_require=True)
 DATABASES['default'].update(db_from_env)
 # DATABASES = {
 #     'default': {
