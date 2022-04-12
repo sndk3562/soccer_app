dcbc31c0f4421c4b824480a03cdb6312cd7845b0
sndk3562 <nekonoinotora@gmail.com>
2022-04-12 17:49:37 +0900

    u

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
-from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +28,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
