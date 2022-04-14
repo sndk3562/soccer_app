94715ab535f50fa8a6000c2eb962379d070de185
sndk3562 <nekonoinotora@gmail.com>
2022-04-14 17:57:57 +0900

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_URL = '/static/'
 STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
